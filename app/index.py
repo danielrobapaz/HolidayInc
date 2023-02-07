3d2c19bee89a2e2ee74602486a5049ea8ced6911
Daniel Robayo <87443087+danielrobapaz@users.noreply.github.com>
2023-02-07 19:46:14 -0400

    adding index blueprint

---
+++ app/index.py
@@ -0,0 +1,11 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from app.auth import login_required
+from app.db import get_db
+
+
+# create the blueprint for the 'index'
+bp = Blueprint('index', __name__)
