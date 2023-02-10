b90605d2c8d0c0d929a350ec9ce1bb67c2302d0d
Daniel Robayo <87443087+danielrobapaz@users.noreply.github.com>
2023-02-09 21:44:55 -0400

    aprove a reject button works

--- app/user.py
+++ app/user.py
@@ -14,9 +14,27 @@ bp = Blueprint('user', __name__, url_prefix='/index')
 @bp.route('/', methods=('POST', 'GET'))
 @login_required
 def index():
-    if request.method == 'POST':
-        pass
+    db = get_db()
 
+    if request.method == 'POST':
+        if 'create' in request.form:
+            pass
+        elif 'aprove' in request.form:
+            id = request.form['aprove']
+            db.execute(
+                'UPDATE user SET auth = 1 WHERE id = ?',
+                (id,)
+            )
+            db.commit()
+        elif 'reject' in request.form:
+            id = request.form['reject']
+            db.execute(
+                'DELETE FROM user WHERE id = ?',
+                (id,)
+            )
+            db.commit()
+
+    
     db = get_db()
     users = db.execute(
         'SELECT id, username FROM user WHERE auth = 0'
