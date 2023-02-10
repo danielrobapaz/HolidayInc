5eadbb81d4d917948aced0d93b6828c2eea56e8e
Jorge Correia <14-10254@usb.ve>
2023-02-10 03:19:33 -0400

    Updated methods for setup tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,17 +1,18 @@
 import unittest
 from flask import g, session
 from app import create_app
-from app.db import init_db,get_db,close_db
-
+from app.db import init_db_command,get_db,close_db
+import click
 class BaseTestsClass(unittest.TestCase):
 
     def setUp(self) -> None:
         self.app = create_app()
         self.client = self.app.test_client()
-
         with self.app.app_context():
-            init_db()
-            get_db()
+            try: 
+                init_db_command()
+            except:
+                pass
 
     def tearDown(self):
         with self.app.app_context():
