5d73d13fd8063a0537607c49b3a6f75bc854d4ec
Jorge Correia <14-10254@usb.ve>
2023-02-23 11:07:56 -0400

    Updated schema. Added logger, systems, events and user_proyect tables

--- app/schema.sql
+++ app/schema.sql
@@ -12,6 +12,38 @@ CREATE TABLE user (
     auth INTEGER
 );
 
+CREATE TABLE user_proyect (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    userid INTEGER NOT NULL,
+    proyectid INTEGER NOT NULL,
+    FOREIGN KEY (userid) references user(id),
+    FOREIGN KEY (proyectid) references proyect(id)
+);
+
+CREATE TABLE logger (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    eventid INTEGER NOT NULL,
+    date TEXT NOT NULL,
+    user TEXT NOT NULL,
+    system TEXT NOT NULL,
+    log_text TEXT NOT NULL,
+    FOREIGN KEY (user) references user(username),
+    -- FOREIGN KEY (system) references systems(id),
+    -- FOREIGN KEY (eventid) references events(id)
+);
+
+CREATE TABLE systems {
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NO NULL
+}
+
+CREATE TABLE events {
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    system TEXT NOT NULL,
+    FOREIGN KEY (system) REFERENCES systems(id)
+}
+
 CREATE TABLE proyect (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     description TEXT UNIQUE NOT NULL,
