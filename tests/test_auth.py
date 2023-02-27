5932bd47b0c1f7f6cd99c241684341fd1fd99d4e
Jorge Correia <14-10254@usb.ve>
2023-02-27 03:05:06 -0400

    Added tests: - User Change Project - Root Delete Project - Change User Role - Delete User

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -173,15 +173,15 @@ class LoginTest(BaseTestsClass):
         html = res.get_data(as_text=True)
         assert "Welcome, root" in html
 
-    def test_rootCreateProject(self):
+    def test_rootCreateProject(self,name='proyect1'):
         print("rootCreateProject\n\n")
         self.test_loginRoot()
-        with self.app.app_context():
-            db = get_db()
-            assert db.execute("select * from proyect").fetchone() is None
-
+        if name != 'proyect2':
+            with self.app.app_context():
+                db = get_db()
+                assert db.execute("select * from proyect where description = 'proyect1' ").fetchone() is None
         res = self.client.post('/createProyect', data={
-            'description':'proyect1',
+            'description':name,
             'starting-date':'2023-01-26',
             'end-date':'2023-02-26'
         }, follow_redirects=True)
@@ -372,3 +372,95 @@ class LoginTest(BaseTestsClass):
             assert data['end'] == datetime(2023,5,26).date() and data['start'] == datetime(2023,4,26).date()
         assert res.status_code == 200
         assert res.request.path == '/user/root'
+
+    def test_rootDeleteProject(self):
+        self.test_rootCreateProject()
+        print("rootDeleteProject\n\n")
+        with self.app.app_context():
+            db = get_db()
+            assert db.execute("select * from proyect where id = 1").fetchone() is not None
+        with self.client.session_transaction() as session:
+            session['modify_proyect'] = '1'
+        res = self.client.post('/modifyProyect', data={
+            'delete':'delete'
+        }, follow_redirects=True)
+        
+        with self.app.app_context():
+            db = get_db()
+            assert db.execute("select * from proyect where description = 'proyect1'").fetchone() is None
+        assert res.status_code == 200
+        assert res.request.path == '/user/root'
+
+
+    def test_userChangeProyect(self):
+        print("rootUserChangeProyect\n\n")
+        self.test_rootCreateUser()
+        self.test_rootCreateProject('proyect2')
+        with self.app.app_context():    
+            db = get_db()
+            p1 = db.execute("select * from proyect where description = 'proyect1'").fetchone()
+            p2 = db.execute("select * from proyect where description = 'proyect2'").fetchone()
+            u1 = db.execute("select proyId from user where id = '2'").fetchone()
+            assert p1 is not None and p2 is not None
+            assert u1[0] == 1
+        with self.client.session_transaction() as session:
+            session['modify_user'] = '2'
+        res = self.client.post('/modifyUser/changeProyect', data={
+            'proyect':'2',
+        }, follow_redirects=True)
+        with self.app.app_context():    
+            db = get_db()
+            p1 = db.execute("select * from proyect where description = 'proyect1'").fetchone()
+            p2 = db.execute("select * from proyect where description = 'proyect2'").fetchone()
+            u1 = db.execute("select proyId from user where id = '2'").fetchone()
+            assert p1 is not None and p2 is not None
+            assert u1[0] == 2
+        assert res.status_code == 200
+        assert res.request.path == '/user/root'
+
+    def test_userDeleteUser(self):
+            print("rootUserDeleteUser\n")
+            self.test_rootCreateUser()
+            with self.app.app_context():    
+                db = get_db()
+                u1 = db.execute("select * from user where id = '2'").fetchone()
+                assert u1 is not None
+            with self.client.session_transaction() as session:
+                session['modify_user'] = '2'
+            res = self.client.post('/modifyUser', data={
+                'delete':'delete',
+            }, follow_redirects=True)
+            with self.app.app_context():    
+                db = get_db()
+                u1 = db.execute("select * from user where id = '2'").fetchone()
+                assert u1 is None
+            assert res.status_code == 200
+            assert res.request.path == '/user/root'
+
+    def test_userChangeRole(self):
+            print("rootUserChangeRole\n")
+            self.test_rootCreateUser()
+            with self.app.app_context():    
+                db = get_db()
+                u1 = db.execute("select * from user where id = 2").fetchone()
+                db.execute("INSERT INTO roles (name, description) VALUES ('mechanic_sup', 'Supervisor del area de mecanica')")
+                db.commit()
+                r1 = db.execute("select * from roles where id = 1").fetchone()
+                r2 = db.execute("select * from roles where id = 2").fetchone()
+                assert u1 is not None and r1 is not None and r2 is not None
+                assert u1['role'] == 'op_manager'
+            with self.client.session_transaction() as session:
+                session['modify_user'] = '2'
+            res = self.client.post('/modifyUser/changeRole', data={
+                'select':'mechanic_sup',
+            }, follow_redirects=True)
+            with self.app.app_context():    
+                db = get_db()
+                u1 = db.execute("select * from user where id = 2").fetchone()
+                r1 = db.execute("select * from roles where id = 1").fetchone()
+                r2 = db.execute("select * from roles where id = 2").fetchone()
+                print(u1['role'])
+                assert u1 is not None and r1 is not None and r2 is not None
+                assert u1['role'] == 'mechanic_sup'
+            assert res.status_code == 200
+            assert res.request.path == '/user/root'
