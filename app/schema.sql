91e8f1809f33ff940c267bf80db7842ff243842e
Daniel Robayo <87443087+danielrobapaz@users.noreply.github.com>
2023-03-08 11:03:47 -0400

    new data base scheema, create clients, cars, carBrands and carModels tables

--- app/schema.sql
+++ app/schema.sql
@@ -3,6 +3,10 @@ DROP TABLE IF EXISTS logger;
 DROP TABLE IF EXISTS proyect;
 DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS proyectStatus;
+DROP TABLE IF EXISTS clients;
+DROP TABLE IF EXISTS cars;
+DROP TABLE IF EXISTS carBrands;
+DROP TABLE IF EXISTS carModels;
 
 CREATE TABLE user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -43,4 +47,42 @@ CREATE TABLE roles (
 CREATE TABLE proyectStatus (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL
+);
+
+CREATE TABLE clients (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    dni TEXT NOT NULL,
+    firstname TEXT NOT NULL,
+    secondname TEXT NOT NULL,
+    birthday TEXT NOT NULL,
+    tlf TEXT NOT NULL,
+    mail TEXT NOT NULL,
+    adress TEXT NOT NULL
+);
+
+CREATE TABLE cars (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    ownerId INTEGER NOT NULL,
+    plaque TEXT NOT NULL,
+    brandId TEXT NOT NULL,
+    modelId TEXT NOT NULL,
+    year INTEGER NOT NULL,
+    bodyWorkSerial TEXT NOT NULL,
+    motorSerial TEXT NOT NULL,
+    color TEXT NOT NULL,
+    problem TEXT NOT NULL,
+
+    FOREIGN KEY (ownerId) REFERENCES clients(id),
+    FOREIGN KEY (brandId) REFERENCES carBrands(id),
+    FOREIGN KEY (modelId) REFERENCES carModels(id)
+);
+
+CREATE TABLE carBrands (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    brandName TEXT NOT NULL
+);
+
+CREATE TABLE carModels (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    modelName TEXT NOT NULl
 );
