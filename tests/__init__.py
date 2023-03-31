6c5b87e7242b40141de561a7976caae054b911ca
Jorge Correia <14-10254@usb.ve>
2023-03-31 05:23:33 -0400

    Updated init file

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
 import unittest
-from flask import g, session
+# from flask import g, session
 from app import create_app
-from app.db import init_db_command,get_db,close_db
+from app.db import init_db_command,close_db
 #import click
 from selenium import webdriver
 from flask import session
@@ -9,8 +9,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from . import page
-class BaseTestsClass(unittest.TestCase):
+
+class SeleniumClass(unittest.TestCase):
 
     def setUp(self) -> None:
         self.app = create_app()
@@ -18,12 +18,30 @@ class BaseTestsClass(unittest.TestCase):
         self.driver = webdriver.Firefox()
         with self.app.app_context():
             try: 
-                init_db_command()
+                init_db_command('')
             except:
                 pass
 
-   
     def tearDown(self):
         with self.app.app_context():
             close_db()
-        self.driver.close()    
+        self.driver.close()
+
+class UnitTestClass(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.app = create_app()
+        self.client = self.app.test_client()
+        with self.app.app_context():
+            try: 
+                init_db_command('')
+            except:
+                pass
+
+    def tearDown(self):
+        with self.app.app_context():
+            close_db()
+
+
+if __name__ == "__main__":
+    unittest.main() 
