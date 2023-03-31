eec5e2be4d783998dbb8e5a62f084064d8a4701e
Jorge Correia <14-10254@usb.ve>
2023-03-31 13:40:01 -0400

    Fixed aprove registered user

--- tests/test_seleniumUserTests.py
+++ tests/test_seleniumUserTests.py
@@ -8,11 +8,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
+from .test_seleniumRegisterTests import *
+from .test_seleniumProjectTests import *
 
 class seleniumUserTests(SeleniumClass):
 
     def test_rootRejectUser(self):
-        registerTests.test_registerNonRegisteredUser(self)
+        seleniumRegisterTests.test_registerNonRegisteredUser(self)
         self.driver.get("http://localhost:5000/auth/login")
         assert "Log In" in self.driver.title
         passwd = self.driver.find_element(By.ID, "password")
@@ -42,42 +44,42 @@ class seleniumUserTests(SeleniumClass):
 
 
 
-         # def test_rootAproveUser(self):
-    #     self.test_registerNonRegisteredUser()
-    #     self.test_RootCreateProyect()
-    #     self.driver.get("http://localhost:5000/auth/login")
-    #     assert "Log In" in self.driver.title
-    #     passwd = self.driver.find_element(By.ID, "password")
-    #     passwd.clear()
-    #     passwd.send_keys("root")
-    #     user = self.driver.find_element(By.ID, "username")
-    #     user.clear()
-    #     user.send_keys("root")
-    #     user.send_keys(Keys.RETURN)
-    #     wait = WebDriverWait(self.driver, 10)
-    #     wait.until(EC.url_to_be("http://localhost:5000/user/root"))
-    #     actualUrl = "http://localhost:5000/user/root"
-    #     expectedUrl= self.driver.current_url
-    #     self.assertEqual(expectedUrl,actualUrl)
-    #     user_button = self.driver.find_element(By.ID, "user")
-    #     user_button.click()
-    #     wait.until(EC.url_to_be("http://localhost:5000/root/users"))
-    #     actualUrl = "http://localhost:5000/root/users"
-    #     expectedUrl= self.driver.current_url
-    #     self.assertEqual(expectedUrl,actualUrl)
-    #     reject_button = self.driver.find_element(By.NAME, "aprove")
-    #     reject_button.click()
-    #     wait.until(EC.url_to_be("http://localhost:5000/root/aproveUser"))
-    #     actualUrl = "http://localhost:5000/root/aproveUser"
-    #     expectedUrl= self.driver.current_url
-    #     self.assertEqual(expectedUrl,actualUrl)
-    #     roleSelect = Select(self.driver.find_element(By.NAME, "role"))
-    #     roleSelect.select_by_visible_text("Gerente de operaciones")
-    #     proyectSelect = Select(self.driver.find_element(By.NAME, "proyect"))
-    #     proyectSelect.select_by_visible_text("testproyect")
-    #     aproveButton = self.driver.find_element(By.NAME, "aprove")
-    #     aproveButton.click()
-    #     wait.until(EC.url_to_be("http://localhost:5000/user/root"))
-    #     actualUrl = "http://localhost:5000/user/root"
-    #     expectedUrl= self.driver.current_url
-    #     self.assertEqual(expectedUrl,actualUrl)
+    def test_rootAproveUser(self):
+         seleniumRegisterTests.test_registerNonRegisteredUser(self)
+         seleniumProjectTests.test_RootCreateProyect(self)
+         self.driver.get("http://localhost:5000/auth/login")
+         assert "Log In" in self.driver.title
+         passwd = self.driver.find_element(By.ID, "password")
+         passwd.clear()
+         passwd.send_keys("root")
+         user = self.driver.find_element(By.ID, "username")
+         user.clear()
+         user.send_keys("root")
+         user.send_keys(Keys.RETURN)
+         wait = WebDriverWait(self.driver, 10)
+         wait.until(EC.url_to_be("http://localhost:5000/user/root"))
+         actualUrl = "http://localhost:5000/user/root"
+         expectedUrl= self.driver.current_url
+         self.assertEqual(expectedUrl,actualUrl)
+         user_button = self.driver.find_element(By.ID, "user")
+         user_button.click()
+         wait.until(EC.url_to_be("http://localhost:5000/root/users"))
+         actualUrl = "http://localhost:5000/root/users"
+         expectedUrl= self.driver.current_url
+         self.assertEqual(expectedUrl,actualUrl)
+         reject_button = self.driver.find_element(By.NAME, "aprove")
+         reject_button.click()
+         wait.until(EC.url_to_be("http://localhost:5000/root/aproveUser"))
+         actualUrl = "http://localhost:5000/root/aproveUser"
+         expectedUrl= self.driver.current_url
+         self.assertEqual(expectedUrl,actualUrl)
+         roleSelect = Select(self.driver.find_element(By.NAME, "role"))
+         roleSelect.select_by_visible_text("Gerente de operaciones")
+         proyectSelect = Select(self.driver.find_element(By.NAME, "proyect"))
+         proyectSelect.select_by_visible_text("testproyect")
+         aproveButton = self.driver.find_element(By.NAME, "aprove")
+         aproveButton.click()
+         wait.until(EC.url_to_be("http://localhost:5000/user/root"))
+         actualUrl = "http://localhost:5000/user/root"
+         expectedUrl= self.driver.current_url
+         self.assertEqual(expectedUrl,actualUrl)
